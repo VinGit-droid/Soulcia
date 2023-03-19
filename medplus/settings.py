a6370cdf41ceae61b9934829c3deec2cfae5451b
VinGit-droid <vinay.karna2043@gmail.com>
2023-03-19 19:19:37 +0530

    First commit

--- medplus/settings.py
+++ medplus/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r7e)4n&9ipp#o$c0@uocxyfo9lrn-5v^66zbq7r5^(2st8_^m-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
--- medplus/urls.py
+++ medplus/urls.py
@@ -20,6 +20,6 @@ from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.homepage),
+    path('', views.homepage),
 
 ]
